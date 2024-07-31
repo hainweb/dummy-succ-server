557260138fe1005c74d688aad8f41109b99aa95e
hainweb <hainofficialweb@gmail.com>
2024-07-31 12:56:05 +0530

    Update userRoutes.js

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -7,10 +7,14 @@ const {
 
 const { protect } = require("../middleware/authMiddleware");
 
+
 const Router = express.Router();
 
 Router.post("/login", loginController);
 Router.post("/register", registerController);
 Router.get("/fetchUsers", protect, fetchAllUsersController);
+
+
+
 
 module.exports = Router;
